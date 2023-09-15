267545607256ca87a0f6db241a5a85d40c48ac75
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-09-15 20:55:10 +0300

    Decorator

--- main.go
+++ main.go
@@ -2,11 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strings"
 )
 
+type Modifier interface {
+	Modify() string
+}
+
+type Original struct {
+	Value string
+}
+
+func (o *Original) Modify() string {
+	return o.Value
+}
+
+// Upper возвращает строку в верхнем регистре.
+type Upper struct {
+	modifier Modifier
+}
+
+func (u *Upper) Modify() string {
+	return strings.ToUpper(u.modifier.Modify())
+}
+
+// Replace заменяет строки old на new.
+type Replace struct {
+	modifier Modifier
+	old      string
+	new      string
+}
+
+func (r *Replace) Modify() string {
+	return strings.Replace(r.modifier.Modify(), r.old, r.new, -1)
+}
+
+func DemoDecorator() {
+	original := &Original{Value: "Привет, гофер!"}
+	log.Println(original.Modify())
+	replace := &Replace{
+		modifier: original,
+		old:      "гофер",
+		new:      "мир",
+	}
+	upper := &Upper{
+		modifier: replace,
+	}
+	log.Println(upper.Modify())
+}
+
 func main() {
 	border("Decorator")
+	DemoDecorator()
 }
 
 func border(name string) {
