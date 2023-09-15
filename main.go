2d1778093d118dc77820fc2af187248034293427
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-09-15 21:35:58 +0300

    Adapter

--- main.go
+++ main.go
@@ -52,9 +52,57 @@ func DemoDecorator() {
 	log.Println(upper.Modify())
 }
 
+// JSONData — интерфейс для декодирования JSON.
+type JSONData interface {
+	DecodeJSON() interface{}
+}
+
+// YAMLData — интерфейс для декодирования YAML.
+type YAMLData interface {
+	DecodeYAML() interface{}
+}
+
+type Client struct {
+	Data interface{}
+}
+
+func (client *Client) Decode(input JSONData) {
+	client.Data = input.DecodeJSON()
+}
+
+type Adapter struct {
+	yaml YAMLData
+}
+
+func (a *Adapter) DecodeJSON() interface{} {
+	return a.yaml.DecodeYAML()
+}
+
+func Load(client *Client, input YAMLData) {
+	adapter := &Adapter{
+		yaml: input,
+	}
+	client.Decode(adapter)
+}
+
+type someYaml struct{}
+
+func (s someYaml) DecodeYAML() interface{} {
+	return "Some YAML"
+}
+
+func DemoAdapter() {
+	client := &Client{}
+	input := someYaml{}
+	Load(client, input)
+	fmt.Printf("json client: %v, yaml: %v\n", client.Data, input.DecodeYAML())
+}
+
 func main() {
 	border("Decorator")
 	DemoDecorator()
+	border("Adapter")
+	DemoAdapter()
 }
 
 func border(name string) {
